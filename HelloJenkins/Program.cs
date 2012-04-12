23af84576696665c0086100c30b993181413f7d9
Akhil Patel <akhil.patel@gmail.com>
2012-04-12 16:22:44 -0700

    updating console output

--- HelloJenkins/Program.cs
+++ HelloJenkins/Program.cs
@@ -9,7 +9,7 @@ namespace HelloJenkins
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Jenkins!");
+            Console.WriteLine("Hello, Jenkins from GitHub!");
         }
     }
 }
