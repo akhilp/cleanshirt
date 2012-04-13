c0cf9135a5c34ec3f3a78637651304e33a0b02a9
Andrew Bose <aljbose@gmail.com>
2012-04-12 20:00:25 -0500

    Added some files for test project that I accidently did not ad before.

---
+++ HelloJenkinsTest/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace HelloJenkinsTest
+{
+    [TestFixture]
+    public class Test
+    {
+        public void TestMethod()
+        {
+            Assert.True(true);
+        }
+    }
+}
