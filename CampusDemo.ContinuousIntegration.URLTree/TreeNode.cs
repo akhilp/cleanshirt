2bbc6ed54271a493cc7240123620e1a918d813b9
Andrew Bose <aljbose@gmail.com>
2012-04-25 20:21:21 -0500

    changed NodeList to a node speed dictionary

--- CampusDemo.ContinuousIntegration.URLTree/TreeNode.cs
+++ CampusDemo.ContinuousIntegration.URLTree/TreeNode.cs
@@ -2,38 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SpeedCollections;
 
 namespace CampusDemo.ContinuousIntegration.URLTree
 {
     public class TreeNode
     {
         public String payload { get; set; }
-        public char Character { get; set; }
-        public List<TreeNode> NodeList { get; set; }
-
+        public char Character { get; set; }        
+        public SpeedDictionary<char, TreeNode> NodeDictionary { get; set; }
         public TreeNode()
         {
-            NodeList = new List<TreeNode>();
+            NodeDictionary = new SpeedDictionary<char, TreeNode>();
         }
 
         public void AddNode(TreeNode newNode)
         {
-            this.NodeList.Add(newNode);
+            this.NodeDictionary.Add(newNode.Character, newNode);
         }
 
         public TreeNode Contains(char character)
         {
             TreeNode resultNode = null;
 
-            for (int i = 0; i < NodeList.Count; i++)
+            if (NodeDictionary.Contains(character))
             {
-                if (this.NodeList[i].Character == character)
-                {
-                    resultNode = this.NodeList[i];
-                    break;
-                }
+                resultNode = NodeDictionary.Retreive(character);
             }
-
+            
             return resultNode;
         }
     }
